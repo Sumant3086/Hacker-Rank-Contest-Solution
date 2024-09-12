c24297adacab12a05892f50329dfeb383659812e
Sumant <sumantyadav3086@gmail.com>
2024-09-12 17:58:16 +0530

    HackkerRank

---
+++ AngryProfessor.cpp
@@ -0,0 +1,152 @@
+// A Discrete Mathematics professor has a class of students. Frustrated with their lack of discipline, the professor decides to cancel class if fewer than some number of students are present when class starts. Arrival times go from on time () to arrived late ().
+
+// Given the arrival time of each student and a threshhold number of attendees, determine if the class is cancelled.
+
+// Example
+
+
+
+
+// The first  students arrived on. The last  were late. The threshold is  students, so class will go on. Return YES.
+
+// Note: Non-positive arrival times () indicate the student arrived early or on time; positive arrival times () indicate the student arrived  minutes late.
+
+// Function Description
+
+// Complete the angryProfessor function in the editor below. It must return YES if class is cancelled, or NO otherwise.
+
+// angryProfessor has the following parameter(s):
+
+// int k: the threshold number of students
+// int a[n]: the arrival times of the  students
+// Returns
+
+// string: either YES or NO
+// Input Format
+
+// The first line of input contains , the number of test cases.
+
+// Each test case consists of two lines.
+
+// The first line has two space-separated integers,  and , the number of students (size of ) and the cancellation threshold.
+// The second line contains  space-separated integers () that describe the arrival times for each student.
+
+// Constraints
+
+// Sample Input
+
+// 2
+// 4 3
+// -1 -3 4 2
+// 4 2
+// 0 -1 2 1
+// Sample Output
+
+// YES
+// NO
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'angryProfessor' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. INTEGER k
+ *  2. INTEGER_ARRAY a
+ */
+
+string angryProfessor(int k, vector<int> a) {
+    int atten=0;
+    for(int i:a){
+        if(i<=0)atten++;
+    }
+    return atten>=k?"NO":"YES";
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string t_temp;
+    getline(cin, t_temp);
+
+    int t = stoi(ltrim(rtrim(t_temp)));
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string first_multiple_input_temp;
+        getline(cin, first_multiple_input_temp);
+
+        vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+        int n = stoi(first_multiple_input[0]);
+
+        int k = stoi(first_multiple_input[1]);
+
+        string a_temp_temp;
+        getline(cin, a_temp_temp);
+
+        vector<string> a_temp = split(rtrim(a_temp_temp));
+
+        vector<int> a(n);
+
+        for (int i = 0; i < n; i++) {
+            int a_item = stoi(a_temp[i]);
+
+            a[i] = a_item;
+        }
+
+        string result = angryProfessor(k, a);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
